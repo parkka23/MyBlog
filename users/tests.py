a500710ac8fb2fb506c8d8693958b3f37c5eca66
ksenia <park.ksenia23@gmail.com>
2023-11-30 11:49:54 +0600

    added filter, search, localization

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.register_url=reverse('sign_up')
+        # self.user={
+        #     'email':'test@gmail.com',
+        #     'username':'username',
+        #     'password':'password',
+        #     ''
+        #
+        # }
+        return super().setUp()
+
+class RegisterTest(BaseTest):
+    def test_can_view_page_correctly(self):
+        response=self.client.get(self.register_url)
+        self.assertEqial(response.status_code,200)
+        self.assertTemplateUser(response,'user/sign_up.html')
